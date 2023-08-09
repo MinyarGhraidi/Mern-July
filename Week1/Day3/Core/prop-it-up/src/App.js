5921b39ab289dfcd48492708f9c898e604bcb8e9
HP <minyarghraidi2021@gmail.com>
2023-08-09 12:00:04 +0100

    prop-it-up

---
+++ Week1/Day3/Core/prop-it-up/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+
+import React from 'react';
+import PersonCard from './components/personCard';
+
+
+// App component
+function App() {
+  var user = [
+    {"firstName": "Jane", "lastName":"Doe","age":45,"hairColor":"Black"},
+  {"firstName": "John", "lastName":"Smith","age":88,"hairColor":"Brown"},
+ { "firstName": "Millard", "lastName":"Fillmore","age":50,"hairColor":"Brown"},
+  {"firstName": "", "lastName":"Smith","age":62,"hairColor":"Brown"}
+]
+ 
+return (
+  <div className="App">
+   { user.map(person =>{
+    return<PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor}/>})}
+  </div>
+);
+}
+
+
+
+
+
+
+
+
+export default App;
