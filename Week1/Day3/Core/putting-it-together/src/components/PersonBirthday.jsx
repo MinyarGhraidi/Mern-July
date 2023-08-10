98e220b9378e450c8357b3e8d6797a710671c445
HP <minyarghraidi2021@gmail.com>
2023-08-10 11:10:44 +0100

    putting-it-together

---
+++ Week1/Day3/Core/putting-it-together/src/components/PersonBirthday.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+
+class PersonBirthday extends Component {
+    constructor(props){
+    // const [age, setAge] = useState(0);
+
+        super(props);
+        this.state = {
+            age: this.props.age
+          };
+    }
+  
+
+        
+    
+  render() {
+    const AgeCounter=()=>  {
+        this.setState({age:this.props.age + 1});
+    }
+
+   
+    
+
+            
+          
+      
+    return (
+      <div className="person-card">
+        <h2>{this.props.firstName} {this.props.lastName}</h2>
+        <p>Age: {this.state.age}</p>
+        <p>Hair Color: {this.props.hairColor}</p>
+       
+      <button onClick={AgeCounter}>Augmenter l'âge</button>
+      </div>
+  );
+      
+
+    }}
+
+export default PersonBirthday;
